76c130fdbef7902a7a7e25735ff9dca2ec3a0693
Yanis <yanis.bounazef@monboreal.ca>
2026-04-21 01:24:20 -0400

    Nettoyage de main pour preparation de la couche services

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-from data.dao_salle import ajouter_salle, modifier_salle
-from models.salle import Salle
-
-# Ajouter une salle
-salle1 = Salle("B101", "Salle informatique", "Laboratoire", 25)
-ajouter_salle(salle1)
-
-# Modifier la salle
-modifier_salle("B101", "Salle informatique MODIF", "Laboratoire", 30)
+print("Projet gestion des salles lancé")
