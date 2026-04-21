740066ab0406a84896934516abd50e5211fb0591
Yanis <yanis.bounazef@monboreal.ca>
2026-04-21 01:31:01 -0400

    Test de la couche services pour la gestion des salles

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Projet gestion des salles lancé")
+from services_salle import creer_salle, afficher_salles, modifier_une_salle
+
+creer_salle("B200", "Salle test", "Classe", 40)
+modifier_une_salle("B200", "Salle test modifiee", "Classe", 45)
+afficher_salles()
