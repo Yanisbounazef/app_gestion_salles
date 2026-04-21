9b9aa26ec56922474436dc3eef56c55bcc7e7b5f
Yanis <yanis.bounazef@monboreal.ca>
2026-04-21 00:22:53 -0400

    Nettoyage du fichier main apres test de connexion

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 from data.dao_salle import connecter_db
 
-print("Avant connexion")
-
-try:
-    conn = connecter_db()
-    print("Connexion réussie")
-except Exception as e:
-    print("Erreur :", e)
-
-print("Fin du programme")
+conn = connecter_db()
+print("Connexion réussie")
