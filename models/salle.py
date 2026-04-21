a65bb304ef72cc05619a3a97947437fc31af922c
Yanis <yanis.bounazef@monboreal.ca>
2026-04-21 00:24:21 -0400

    Creation de la classe Salle

---
+++ models/salle.py
@@ -0,0 +1,6 @@
+class Salle:
+    def _init_(self, code, description, categorie, capacite):
+        self.code = code
+        self.description = description
+        self.categorie = categorie
+        self.capacite = capacite
