23b68de1f0ba14251984dffc45cc0bbc0c59dec4
Yanis <yanis.bounazef@monboreal.ca>
2026-04-21 01:10:01 -0400

    Modification de main pour tester la suppression d une salle

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from data.dao_salle import recuperer_salles
 
-salles = recuperer_salles()
+from data.dao_salle import supprimer_salle
 
-for salle in salles:
-    print("Code :", salle.code)
-    print("Description :", salle.description)
-    print("Categorie :", salle.categorie)
-    print("Capacite :", salle.capacite)
-    print("-------------------")
+supprimer_salle("B101")
